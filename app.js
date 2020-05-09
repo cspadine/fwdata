361b6c16bf102e9d39b7e61d66a22003de7927e1
Carrie Spadine <cspadine@gmail.com>
2020-05-08 20:22:39 -0400

    Added files and changes required for heroku

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.use(helmet());
 
 //mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://cspadine:Kr1123Cbi@cluster0-kxhsq.mongodb.net/Project0?retryWrites=true&w=majority'
+const dev_db_url = 'mongodb+srv://cspadine:Kr1123Cbi@cluster0-kxhsq.mongodb.net/Project0?retryWrites=true&w=majority'
  ;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
