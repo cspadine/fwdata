924658cf94be973ed17c5b68bde7b638004c2d42
Carrie Spadine <cspadine@gmail.com>
2020-08-27 17:14:11 -0400

    Adds morpheme ids

--- controllers/secureDataController.js
+++ controllers/secureDataController.js
@@ -6,7 +6,11 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const getQueryList = require('../helper/queryTerms.js').getQueryList;
 const getLexQueryList = require('../helper/lexQueryTerms.js').getLexQueryList;
-
+const validator = require('express-validator');
+const morphCleaner = require('../helper/lexiconFormatting.js').morphCleaner;
+const CFEloop = require('../helper/lexiconFormatting.js').CFEloop;
+const wordsToMorphs = require('../helper/lexiconFormatting.js').wordsToMorphs;
+const checkForExisting = require('../helper/lexiconFormatting.js').checkForExisting;
 //import the JSON web token key from the .env file for authetification
 const jwtKey = process.env.JWT_SECRET_WORD;
 
@@ -49,6 +53,9 @@ exports.lexicon_search = function(req, res, next){
 })
 }
 
+
+
+
 exports.source_get = function(req, res, next){
   const user = ( req.cookies.jwt ? jwt.verify(req.cookies.jwt, jwtKey).user.username : '' );
   res.render('sources',{user:user})
@@ -72,3 +79,98 @@ exports.data_delete_post = function(req, res) {
      });
 
  })}
+
+
+ // Display detail page for a specific lexeme.
+ exports.lexeme_detail = function(req, res) {
+     res.send('NOT IMPLEMENTED: Lexeme detail: ' + req.params.id);
+
+ };
+
+
+//Add a new sentence and the morphemes that compose it
+ exports.create_post = function(req,res) {
+
+   const user = ( req.cookies.jwt ?
+     jwt.verify(req.cookies.jwt, jwtKey).user.username : '' ); //user validation
+   const errors = validator.validationResult(req);
+   const cleanedMorph = morphCleaner(req.body.morph.split(',')); //strip trailing
+   //and leading punctuation
+   let data = new Data({ //create new Data object
+           text: req.body.text,
+           source: req.body.source,
+           ref: req.body.ref,
+           judgment: req.body.judgment,
+           context: req.body.context,
+           gloss: req.body.gloss.split(','),
+           morph: cleanedMorph,
+           trans: req.body.trans,
+           notes: req.body.notes,
+           tags: req.body.tags.split(' '),
+           user: req.user.user._id,
+           lang: req.body.lang,
+           });
+           console.log(data)
+    addLexemes(data.morph, data.gloss, data.user, data.lang) //add lexemes to db
+    .then((output) => data.morpheme_ids = output)//add lexeme ids to the Data object
+    .then(()=> {
+      if ( !errors.isEmpty() ) {
+          res.send({data_list: data, errors: errors.array()});
+       return;
+     }  else {
+       Data.findOne({ 'text': data.text, 'context': data.context, 'lang': data.lang, 'user':data.user})
+           .exec( function(err, found_text ) {
+               if (err) { return next(err); }
+
+               if (found_text) {//display result
+                  res.render('data_form', {title: 'Create Data',
+                  message : "This sentence is already in the database.",
+                  data_list: data, errors: errors.array(),
+                  user: user, 'lang': [data.lang]});
+               }
+               else {
+                   data.save(function (err) {//save data and display result
+                       if (err) {return next(err); }
+                       res.render('data_form', {title: 'Create Data',
+                       message: "", data_list: data,
+                       errors: errors.array(), user: user,
+                       'lang': [data.lang]});
+                   });
+               }
+           });
+    }
+  })
+  };
+
+
+
+
+
+
+
+
+ //addLexemes formats the morphemes and glosses into lexeme objects,
+ //checks if they already exist in the lexicon,
+ //if not, adds them to the lexicon, and then returns a list of the
+ //ids of the lexemes that the sentence contains.
+ const addLexemes = function(morph, gloss, user, lang){
+   return new Promise((resolve, reject) => {
+   let wToM = [] //will contain an array of morphs and an array of glosses
+   //wordsToMorphs splits words and glosses into morphemes and checks that they
+   //have an equal number of elements
+    try{
+      wToM = wordsToMorphs(morph,gloss);
+       }
+     catch (err) {
+       console.log(err)
+     }
+     //if wordsToMorphs successfully returns a value, then proceed to looping
+     //through the morphemes it contains (format of wToM is [[<morphemes>],[<glosses>]])
+     if (wToM.length != 0){
+       const morphlist = wToM[0];
+       const glosslist = wToM[1];
+       //loop through morphemes in morphlist
+ CFEloop(morph, gloss, user, lang).then((output) => resolve(output)).catch((e)=> console.log(e))
+  }
+ })
+  }
--- helper/lexiconFormatting.js
+++ helper/lexiconFormatting.js
@@ -1,29 +1,50 @@
 const Lex = require('../models/lexiconSecure');
 const mongoose = require('mongoose');
-//takes an array of words and an array of glosses,
-//makes sure that the lengths match, and converts it into an array
-//where the individual morphemes are the items.
+
+
+//wordsToMorphs takes an array of words with morpheme boundaries indicated
+//by punctuation (= morph) and an array of gloss items with morpheme boundaries indicated
+//by punctuation corresponding to the relevant word (=gloss)
 module.exports.wordsToMorphs = function (morph,gloss){
-  if (morph.length == gloss.length){
-    let morphEndArray = [];
-    let glossEndArray = [];
-    for (let i = 0 ; i < morph.length; i++){
-      const morphSubArray = morph[i].split(/[-~+()\/\[\]$]/);
-      const glossSubArray = gloss[i].split(/[-~+()\/\[\]$]/);
-      if (morphSubArray.length == glossSubArray.length) {
-        for (let j = 0 ; j < morphSubArray.length ; j++){
-          morphEndArray.push(morphSubArray[j])
-          glossEndArray.push(glossSubArray[j])
-        };
-      };
-    };
-  const morphAndGloss = [morphEndArray, glossEndArray];
-  return morphAndGloss
-  } else {
-    console.log("lengths don't match!")
-    return null
+  //there should be one gloss item for each word; if not, then no information should
+  //be stored in the lexicon based on this data, as it is not clear which
+  //glosses correspond to which morphemes
+    if (morph.length != gloss.length)
+    //throw error if there are not the same number of words as gloss items.
+    {throw 'The words and glosses do not align; no lexical items saved.'}
+    //if number of words is the same as number of gloss, items, proceed
+    else {
+      //create empty arrays that will contain a list of morphemes and glosses
+      let morphEndArray = [];
+      let glossEndArray = [];
+      //loop throught words in morph and gloss
+      for (let i = 0 ; i < morph.length; i++){
+        //split words on punctuation indicating morpheme boundaries
+        const morphSubArray = morph[i].split(/[-~+()\/\[\]$]/);
+        const glossSubArray = gloss[i].split(/[-~+()\/\[\]$]/);
+        //check if there are the same number of morphemes as glosses in each word.
+          if (morphSubArray.length != glossSubArray.length){
+          // if there are not the same number of morphemes as glosses, throw an
+          // error
+          throw `"${morph[i]}" doesn't have the same number of items as its gloss.`;
+            } else {
+              //if there are the same number of morphemes, loop through and add
+              //each morpheme-gloss pair to the corresponding list
+                  for (let j = 0 ; j < morphSubArray.length ; j++){
+                    morphEndArray.push(morphSubArray[j])
+                    glossEndArray.push(glossSubArray[j])
+                  };
+            };
+    }
+    const morphAndGloss = [morphEndArray, glossEndArray];
+    return morphAndGloss
   }
-};
+}
+
+
+
+
+
 
 //strip punctuation and leading/trailing spaces from morph row
 module.exports.morphCleaner = function (array){
@@ -39,7 +60,47 @@ module.exports.morphCleaner = function (array){
 
 
 
+//loops through the list of morphemes and for each morpheme, checks whether it
+//is already in the lexicon, and then saves it if it is not. Returns a list of the
+//ids of every morpheme in the sentence.
+module.exports.CFEloop = function(morphArray, glossArray, user, lang) {
+  //promises will be an array of promize objects
+  let promises = [];
+  //the function itself also returns a promise
+  return new Promise ((resolve, reject) => {
+      //morpheme_ids will be the list of morphemes ids
+      let morpheme_ids = [] //will contain the ids of the morphemes
+      //loop through the list of morphemes, and for each morpheme add as promise
+      //for the result of checkForExisting
+      for (let i = 0; i < morphArray.length; i++){
+        promises.push(checkForExisting(morphArray[i],glossArray[i], user, lang)
+             .then(output => {
 
+       //check for existing always returns an object; if the lexeme already exists,
+       //it will return an object that only contains the id of the existing lexeme.
+       //if the lexeme does not exist, it will return an object that contains
+       //all of the information that will then be saved as a new lexeme.
+               if (Object.keys(output).includes("morph")){ //if output has 'morph'
+               //key, then the lexeme is not yet in the database.
+                 const lexeme = new Lex(output); //creates new Lex object
+                 console.log(lexeme)
+                 lexeme.save(); //saves Lex object
+                 morpheme_ids.push(lexeme._id);//adds newly created id to
+                 //list of morpheme ids
+               } else { //if the lexeme already exists
+                 console.log(output)
+                 morpheme_ids.push(output._id)//adds existing lexeme id to list
+          }
+
+        }).catch((err)=>{
+         reject();
+      })
+    )
+    }
+    //once all of the promises in promises have resolved, return morpheme_ids as the value.
+        Promise.all(promises).then(()=>resolve(morpheme_ids)).catch((e)=> console.log(e))
+  })
+}
 
 
 
@@ -50,22 +111,25 @@ module.exports.morphCleaner = function (array){
 
 //checks whether an entry already exists for this combination of
 //glosses and morphs
-module.exports.checkForExisting = function (morph,gloss,user,lang){
+const checkForExisting = function (morph,gloss,user,lang){
   let lexeme = {
             morph : morph,
             gloss : gloss,
             user : user,
             lang: lang
           };
-  return new Promise (function (resolve, reject){
-    Lex.findOne(lexeme).then(data =>{
-      if (data) {
-        resolve(null)
+return new Promise ( (resolve, reject) => {
+    Lex.findOne(lexeme)
+     .then(data =>{
+       if (data != null) {
+        resolve(data._id)
       } else {
         resolve(lexeme)
       }
-    }).catch((err)=>{
-      reject();
-    })
+ })
+ .catch(function (e) {
+   console.log('Error occurred');
+   console.log(e);
+ })
 })
 }
